7f8e5883f1d4c0f860942a8fb1366d49eed84dee
DESKTOP-3NF0O37\milop <milopalma@hotmail.com>
2017-10-13 12:47:05 -0600

    paquete de entrega 1#

--- index.php
+++ index.php
@@ -4,11 +4,27 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+<head> 
   <title>Sign-Up/Login Form</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <nav style="background: transparent;" ><div  class="container-wide"><div class="row"><div class="col s12 m3" ><a href="/" class=>
+
+</a></div><div class="col s12 m9"><div class="right"><ul><li><a href="/#features">Features</a></li><li><a href="/team">Our Team</a></li><li><a href="https://app.closeheat.com/users/sign_in" class="modal-trigger btn nav-sign-up waves-effect waves-light">Registrar</a></li></ul></div></div></div></div></nav>
+
+
+      </ul>
+    </div>
+  </nav>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">      
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="css/materialize.min.css">
 </head>
+
+<style type="text/css">
+  input{
+    width: auto !important;
+  }
+</style>
 
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
@@ -29,23 +45,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <!DOCTYPE html>
 <html>
 <head>
+        <link rel="stylesheet" href="./css/style.css " type="text/css"  >
   <title>Sign-Up/Login Form</title>
 </head>
-<body >
-        
-          <h1>Bienvenido de vuelta!</h1>
-          
+<style type="text/css"> 
+.nav{
+  background-color: black;
+}
+#email { 
+    width: auto;
+}
+.h1{
+    color: white;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+#cosa{
+    color: white;
+    background-color: black;
+        height: 700px;
+    width: 50%;
+}
+body{ 
+
+       background-size: 50px 100px;
+       background-size: 100% 100%;
+background-repeat : no-repeat; 
+background-attachment : fixed; 
+
+} 
+</style>
+
+<body  background="fondo2.jpg">
+ 
+ <div style="align-content: center;" id="cosa">
+        <h1 style="color: white">Ingresar</h1>
           <form action="index.php" method="post" autocomplete="off"> 
             <label>
                 <p>Email</p>
             </label>
-            <input type="email" required autocomplete="off" name="email"/>
+            <input id="email" type="email" required autocomplete="off" name="email"/>
             <label>
                 <p>Contraseña</p>
             </label>
             <input type="password" required autocomplete="off" name="password"/>
           <button class="btn btn-primary" name="login" />Iniciar Sesion</button>
           </form>
+
+
           <h1>Registrate Gratis</h1>
           
           <form action="index.php" method="post" autocomplete="off">
@@ -58,8 +104,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
               </label>
               <input type="text"required autocomplete="off" name='lastname' />
  
+                      <div class="row">
 
- 
             <label>
                 <p>Email</p>  
             </label>
@@ -69,32 +115,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
             </label>
             <input type="password"required autocomplete="off" name='password'/>
           <button type="submit" class="btn btn-primary" name="register" />Registrarse</button>
-          </form>
+          </form></div>
+ 
 
-    <h1>Blogs Disponibles!</h1>
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+      <script src="js/materialize.min.js"></script>     
+
 </body>
+
 </html>
-<?php 
-require 'db2.php';
- $contenido = $mysqli1->query("SELECT titulo FROM comentarios");
- $contenido1 = $mysqli1->query("SELECT id FROM comentarios");
-$contenido2 = $mysqli1->query("SELECT id, titulo, content, account FROM comentarios");
-$row = array();
-$row2 = array();
-$row3 = array();
-$v='';
-$rows1=$contenido-> num_rows;
-$rows1=$contenido2-> num_rows;
-$strings = array();
-$account=array();
-$comprar='';
-while( $row[] = mysqli_fetch_assoc( $contenido ) );
-while( $row1[] = mysqli_fetch_assoc( $contenido2 ) );
-$_SESSION['av']=array(); 
-for ($i = 0; $i < $rows1; $i++) {
-$enviar="<p><a  href='captcha.php'> ".$row[$i]['titulo']."</a></p>".'by: '.$row1[$i]['account'] ;
-    echo $enviar."  <p>copiar el siguiente codigo para poder acceder al foro:<p>"." "."(".$row1[$i]['id'].")";  
-    }
-?>
